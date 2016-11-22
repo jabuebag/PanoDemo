4c2dff2446b9cbd3166d30c977a4f499fa268ad4
jabuebag <ybagyang@gmail.com>
2016-11-22 11:46:38 -0800

    add change vr mode button

--- PanoDemo/SphereViewController.swift
+++ PanoDemo/SphereViewController.swift
@@ -13,31 +13,60 @@ class SphereViewController: GLKViewController {
     
     var panoramaView = PanoramaView()
     
+    var btnVRMode: UIButton?
+    var btnVRReturn: UIButton?
+    // VR mode status
+    var isVRMode:Bool = false
+    
     override func viewDidLoad() {
         panoramaView?.setImage("test13.jpg")
-        panoramaView?.touchToPan = false          // Use touch input to pan
-        panoramaView?.orientToDevice = true     // Use motion sensors to pan
-        panoramaView?.pinchToZoom = false         // Use pinch gesture to zoom
-        panoramaView?.showTouches = false         // Show touches
-        panoramaView?.setVRModeExt(true)
+        panoramaView?.touchToPan = false
+        panoramaView?.orientToDevice = true
+        panoramaView?.pinchToZoom = false
+        panoramaView?.showTouches = false
+        panoramaView?.setVRModeExt(false)
         self.view = panoramaView
         
-//        let btn: UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
-//        btn.backgroundColor = UIColor.green
-//        btn.setTitle("Click Me", for: .normal)
-//        btn.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-//        btn.tag = 1
-//        self.view.addSubview(btn)
-    }
-    
-    func buttonAction(sender: UIButton!) {
-        let btnsendtag: UIButton = sender
-        if btnsendtag.tag == 1 {
-            //do anything here
-        }
+        // add change to VR mode button
+        addVRModeBtn()
+        // add return back from VR mode button
+        addVRReturnBtn()
     }
     
     override func glkView(_ view: GLKView, drawIn rect: CGRect) {
         panoramaView?.draw()
+    }
+    
+    func addVRModeBtn() {
+        btnVRMode = UIButton(frame: CGRect(x: self.view.frame.size.width-100, y: self.view.frame.size.height-50, width: 100, height: 50))
+        btnVRMode?.setTitle("VR Mode", for: .normal)
+        btnVRMode?.addTarget(self, action: #selector(toVRMode), for: .touchUpInside)
+        self.view.addSubview(btnVRMode!)
+    }
+    
+    func toVRMode(sender: UIButton!) {
+        // panoramaView?.pinchToZoom = false
+        // panoramaView?.touchToPan = false
+        panoramaView?.setVRModeExt(true)
+        isVRMode = true
+        btnVRMode?.isHidden = true
+        btnVRReturn?.isHidden = false
+    }
+    
+    func addVRReturnBtn() {
+        btnVRReturn = UIButton(frame: CGRect(x: self.view.frame.size.width-100, y: 0, width: 100, height: 50))
+        btnVRReturn?.setTitle("return", for: .normal)
+        btnVRReturn?.addTarget(self, action: #selector(outVRMode), for: .touchUpInside)
+        btnVRReturn?.isHidden = true
+        self.view.addSubview(btnVRReturn!)
+    }
+    
+    func outVRMode(sender: UIButton!) {
+        // panoramaView?.pinchToZoom = false
+        // panoramaView?.touchToPan = false
+        panoramaView?.setVRModeExt(false)
+        isVRMode = false
+        btnVRMode?.isHidden = false
+        btnVRReturn?.isHidden = true
     }
 }
