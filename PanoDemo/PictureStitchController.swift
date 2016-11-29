a19955f9e4a0b4d75de33298b110224ee4198591
jabuebag <ybagyang@gmail.com>
2016-11-28 16:45:03 -0800

    add picture stitch controller

---
+++ PanoDemo/PictureStitchController.swift
@@ -0,0 +1,23 @@
+//
+//  PictureStitchController.swift
+//  PanoDemo
+//
+//  Created by Kris Yang on 2016-11-28.
+//  Copyright © 2016 Kris Yang. All rights reserved.
+//
+import UIKit
+
+class PictureStitchController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
