658ab95dc528afb26ee9479522af7fb7b1b0c27f
jabuebag <ybagyang@gmail.com>
2016-11-29 18:42:09 -0800

    add change fade effect

--- PanoDemo/PictureStitchController.swift
+++ PanoDemo/PictureStitchController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class PictureStitchController: UIViewController {
     
+    @IBOutlet weak var originImg: UIImageView!
+    @IBOutlet weak var fadeImg: UIImageView!
+    @IBOutlet weak var fadeBtn: UIButton!
+    
+    let imageUtil = ImageUtil()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        originImg.image = UIImage(cgImage: imageUtil.createFadeImage(500, 1, true)!)
     }
     
     override func didReceiveMemoryWarning() {
@@ -19,5 +26,8 @@ class PictureStitchController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func fadeAction(_ sender: Any) {
+        
+    }
     
 }
